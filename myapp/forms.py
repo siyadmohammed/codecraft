22aaa6ba1f1101aff5f80003e0e7d09aa37d9179
siyadmohammed <siyad.ma.mohammed2001@gmail.com>
2024-09-07 12:06:05 +0530

    07-09-2024

--- myapp/forms.py
+++ myapp/forms.py
@@ -1,7 +1,16 @@
+
 from django import forms
 
 class ContactForm(forms.Form):
-    name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'placeholder': 'Your Name'}))
-    email = forms.EmailField(widget=forms.EmailInput(attrs={'placeholder': 'Your Email'}))
-    subject = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'placeholder': 'Subject'}))
-    message = forms.CharField(widget=forms.Textarea(attrs={'placeholder': 'Message', 'rows': 6}))
+    name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={
+        'autocomplete': 'name'
+    }))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={
+        'autocomplete': 'email'
+    }))
+    subject = forms.CharField(max_length=200, widget=forms.TextInput(attrs={
+        'autocomplete': 'off'
+    }))
+    message = forms.CharField(widget=forms.Textarea(attrs={
+        'autocomplete': 'off'
+    }))
